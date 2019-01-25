864ab8ee8cacd6a64b158a504d336662fd637f7a
Tharindu Muhandiram <tharindu.m@platformer.com>
2019-01-26 05:04:03 +0530

    update nodejs App

--- JS/Node/hello-world-app/index.js
+++ JS/Node/hello-world-app/index.js
@@ -5,7 +5,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end('Hello Kubernetes Sri Lanka\n');
 }).listen(8081);
 
 // Console will print the message
