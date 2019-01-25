236f3869f92748be9130765a56855b6ace1d517c
Tharindu Muhandiram <tharindu.m@platformer.com>
2019-01-26 04:59:59 +0530

    node hello world

---
+++ JS/Node/hello-world-app/index.js
@@ -0,0 +1,12 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://localhost:8081/');
